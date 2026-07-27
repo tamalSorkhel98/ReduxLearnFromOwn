133c9e0b062e00d01453ea67c03540ff83475aaa
Tamal Sorkhel <tamal.sorkhel98@gmail.com>
2026-07-27 19:38:43 +0530

    Delete Basic-Redux directory

--- Basic-Redux/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {configureStore}from "@reduxjs/toolkit"
-import counterSlice from "./featuress/counterSlice"
-export const store = configureStore({
-    reducer:{
-        counter:counterSlice
-    }
-})
