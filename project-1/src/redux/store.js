d37f8684b0e911dd087e8c92aaed6780b9164eb4
Tamal Sorkhel <tamal.sorkhel98@gmail.com>
2026-07-27 19:39:04 +0530

    Delete project-1 directory

--- project-1/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import searchReducer from './features/searchSlice'
-const store = configureStore({
-    reducer:{
-        search:searchReducer
-    }
-})
-export default store
